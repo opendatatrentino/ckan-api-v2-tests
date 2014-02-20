573b3430cf3b1c51b129ff38c8e339004c6476e1
Samuele Santi <redshadow@hackzine.org>
2014-02-20 19:13:49 +0100

    New API key :)

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export CKAN_BASE_URL='http://127.0.0.1:8000'
-export CKAN_API_KEY='2e9d265f-ebb2-47d1-aa2d-4cdd12bb1664'
+export CKAN_API_KEY='5cfe9b53-4173-4c25-9f4d-f9cf3512485e'
 
 exec py.test -v "$@"
