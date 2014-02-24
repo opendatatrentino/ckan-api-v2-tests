df136d4d8c795486f2d123999979212da6f978c7
Samuele Santi <redshadow@hackzine.org>
2014-02-24 12:44:45 +0100

    Now reading API key from .apikey file

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd "$( dirname "$( readlink -f "$BASH_SOURCE" )" )"
+
 export CKAN_BASE_URL='http://127.0.0.1:8000'
-export CKAN_API_KEY='5cfe9b53-4173-4c25-9f4d-f9cf3512485e'
+export CKAN_API_KEY="$( cat .apikey )"
 
 exec py.test -v "$@"
